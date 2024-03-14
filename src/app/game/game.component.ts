7c43a0f9bf5ad05bd6e2068d510006b66ca29014
alfkar <alfkar@student.chalmers.se>
2024-03-14 12:56:38 +0100

    start on hole logic

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HoleComponent } from '../hole/hole.component';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent {
+
+}
