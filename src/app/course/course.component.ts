3ae324b80c04ad650693b7a9629c6eb240f120d5
alfkar <alfkar@student.chalmers.se>
2024-03-15 12:51:12 +0100

    merging

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HoleComponent } from '../hole/hole.component';
 import { CommonModule } from '@angular/common';
+import {}
 
 @Component({
   selector: 'app-course',
@@ -18,10 +19,13 @@ export class CourseComponent {
   ];
 
   players: { name: string, scores: number[] }[] = [];
+  selectedHoleIndex: number | null = null;
 
   constructor() { }
+
   ngOnInit() {
   }
+
   addPlayer(playerName: string) {
     this.players.push({ name: playerName, scores: new Array(this.holes.length).fill(0) });
   }
@@ -29,9 +33,12 @@ export class CourseComponent {
   updatePlayerScore(playerIndex: number, score: number, holeIndex: number) {
     this.players[playerIndex].scores[holeIndex] = score;
   }
-  
 
   getTotalScoreForPlayer(playerIndex: number): number {
     return this.players[playerIndex].scores.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  selectHole(holeIndex: number) {
+    this.selectedHoleIndex = holeIndex;
   }
 }
